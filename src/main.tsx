c8cb87743449925c02456520dac48ddb9a213f53
[dyad] <vasfnegocios@gmail.com>
2026-02-25 18:13:50 -0300

    [dyad] Corrigindo erro de dispatcher do React através de sincronização de versões e rebuild - wrote 4 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./globals.css";
 
-const rootElement = document.getElementById("root");
-if (!rootElement) throw new Error("Failed to find the root element");
-
-createRoot(rootElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById("root");
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
